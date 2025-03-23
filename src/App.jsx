c34e83d2e20ce2284055b4c896886e641c37fe89
Kiran Varma <mkiranvarma123@gmail.com>
2025-03-23 20:21:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+
+function App(){
+
+  const [data, setData] = useState([]);
+
+  const api = "https://picsum.photos/v2/list?limit=25"; 
+
+  useEffect(() => {
+    
+    async function fetchData() {
+      
+      try{
+        let response = await fetch(api);
+        let output = await response.json();
+        setData(output);
+      }
+      
+      catch(err){
+        console.error("Error", err);
+      }
+
+    }
+    fetchData();
+  }, []);
+
+  function copyToClipboard(url){
+    navigator.clipboard.writeText(url);
+    alert("URL copied to clipboard");
+  };
+
+  return (
+
+    <div className="Parent">
+
+      <div className="Child">
+        {
+          data.map(details => 
+            <div key={details.id} className="childCard">
+              
+              <a href={details.download_url} target="_blank">
+                <img src={details.download_url} alt={`By ${details.author}`} className="image" />
+              </a>
+              
+              <div className="childCard2">
+                <p>{details.author}</p>
+                <button className="copyButton" onClick={() => copyToClipboard(details.download_url)} >Copy</button>
+              </div>
+
+            </div>
+          )
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
